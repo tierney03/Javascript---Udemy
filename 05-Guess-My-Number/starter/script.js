f870942fd42e59faf7c44cc37dbc6f49f42f8bb5
tierney03 <tierney_03@mac.com>
2023-01-30 11:19:12 -0600

    Add else ifs and score manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,13 +1,36 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'Correct Number!';
+// document.querySelector('.message').textContent = 'Correct Number!';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
